93304cb3a8e6d41fc06bb43d21b68b0d5e44279d
Rosa <rosa@octahedroid.com>
2020-10-15 12:57:54 -0500

    added a new page to successful message

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledJumbo } from '../styles/components'
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Ya comprate ropa</h2>
+        <small>Tus favoritos</small>
+      </div>
+    </StyledJumbo>
+  )
+}
---
+++ src/pages/successfulPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import SEO from '../components/seo'
+import { Button, Purchase } from '../styles/components'
+import { Link } from 'gatsby'
+
+export default function successfulPage() {
+  return (
+    <div>
+      <SEO title="successful purchase" />
+      <Purchase>
+        <h2>Compra Exitosa </h2>
+        <p>Espero que disfrutes tu ropa </p>
+        <span rol="img" aria-label="emoji ">❤️</span>
+        <Link to='/'>
+          <Button>Volver al catálogo</Button>
+        </Link>
+      </Purchase>
+      
+    </div>
+  )
+}
