a889b0618b21969447130e4a98b26208999541ed
Rosa <rosa@octahedroid.com>
2020-10-21 17:27:51 -0500

    finish styles from car

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,8 +1,9 @@
-  import React, { useContext, useEffect, useState } from "react"
-  import { StyledCart, Button } from "../styles/components"
+/** @jsx jsx */
+import React, { useContext, useEffect, useState } from "react"
   import priceFormat from "../utils/priceFormat"
   import { CarContext } from "../context"
-  import { Link } from "gatsby"
+import { Link } from "gatsby"
+  import { jsx, Box } from 'theme-ui'
   
   export default function Car() {
     const { car } = useContext(CarContext)
@@ -44,20 +45,70 @@
     }
     return (
       <>
-        <StyledCart>
-          <h2>Tu carrito de compras</h2>
+        <Box as="div"
+          __css={{
+            p: "1rem",
+            height: "80vh",
+            overflowY: "scroll",
+            bg: "mateLight"
+          }}
+        >
+          <Box
+            as="h2"
+            __css={{
+              borderBottom: "2px solid black"
+            }}
+          >
+            Tu carrito de compras
+          </Box>
           <table>
             <tbody>
               <tr>
-                <th>Producto</th>
-                <th>Precio</th>
-                <th>Cantidad</th>
-                <th>Total</th>
+                <Box
+                  as="th"
+                  __css={{
+                    color: "#959595",
+                    m: 0,
+                    border: "none"
+                  }}
+                >
+                  Producto
+                </Box>
+                 <Box
+                  as="th"
+                  __css={{
+                    color: "#959595",
+                    m: 0,
+                    border: "none"
+                  }}
+                >
+                  Precio
+                </Box>
+                 <Box
+                  as="th"
+                  __css={{
+                    color: "#959595",
+                    m: 0,
+                    border: "none"
+                  }}
+                >
+                  Cantidad
+                </Box>
+                 <Box
+                  as="th"
+                  __css={{
+                    color: "#959595",
+                    m: 0,
+                    border: "none"
+                  }}
+                >
+                  Total
+                </Box>
               </tr>
               {car.map(item => (
                 <tr key={item.id}>
                   <td>
-                    <img src={item.metadata.img} alt={item.name} />
+                    <img src={item.metadata.img} alt={item.name} sx={{ width:"100px" }} />
                   </td>
                   <td>{priceFormat(item.unit_amount)}</td>
                   <td>{item.quantity}</td>
@@ -66,21 +117,46 @@
               ))}
             </tbody>
           </table>
-          <nav>
-            <div>
-              <h3>Subtotal: </h3>
-              <small>MXN {priceFormat(total)}</small>
+          <nav sx={{display:"flex", flexDirection:"column", alignItems:"flex-end"}}>
+            <div sx={{display:"flex", alignItems:"baseline", width: "250px", justifyContent: "space-between"}}>
+              <h3>Subtotal:</h3>
+              <small sx={{ color:"#505050", fontSize:"0.9rem" }}>MXN {priceFormat(total)}</small>
             </div>
             <div>
               <Link to="/">
-                <Button type="outline">Regresar</Button>
+                <Box as="button" type="outline"
+                  __css={{
+                    cursor: "pointer",
+                    bg: "transparent",
+                    color: "purplePrimary",
+                    fontSize: "0.9rem",
+                    p: "0.6rem 1rem",
+                    fontWeight: 500,
+                    border: "3px solid #665DCA",
+                    borderRadius: "12px"
+                  }}
+                >
+                  Regresar
+                </Box>
               </Link>
-              <Button onClick={handleBuy} disabled={car.length === 0}>
+              <Box as="button"
+                onClick={handleBuy} disabled={car.length === 0}
+                 __css={{
+                    cursor: "pointer",
+                    bg: "#BB757D",
+                    color: "white",
+                    fontSize: "0.9rem",
+                    p: "0.6rem 1rem",
+                    fontWeight: 500,
+                    border: "3px solid #BB757D",
+                    borderRadius: "12px"
+                  }}
+              >
                 Comprar
-              </Button>
+              </Box>
             </div>
           </nav>
-        </StyledCart>
+        </Box>
       </>
     )
   }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { Content, Footer } from "../styles/components"
 import { ThemeProvider } from "theme-ui"
 import theme from "../gatsby-plugin-theme-ui/index"
 import { Box } from "theme-ui"
@@ -15,7 +7,6 @@ import { Box } from "theme-ui"
 
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
 
@@ -23,7 +14,11 @@ const Layout = ({ children }) => {
   return (
     <ThemeProvider ThemeProvider theme={theme}>
       <Header />
-      <Content>
+      <Box as="div"
+        __css={{
+          paddingTop: "4.5rem"
+        }}
+      >
         <main>{children}</main>
         <Box
           as="footer"
@@ -39,7 +34,7 @@ const Layout = ({ children }) => {
         >
           ♡ Fashion
         </Box>
-      </Content>
+      </Box>
     </ThemeProvider>
   )
 }
